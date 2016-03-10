621c479a6e51f776e9e84299e2f0ccd947a54de0
nini <nini@pikicast.com>
2016-03-10 18:22:56 +0900

    main hover추가, 상단 바 스타일 완료

---
+++ javascript/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var speed = 2000;
+  var colors=["#ffbadd","#b0d8fe","#dbb9fd"];
+
+  function flash(index){
+    if (index >= colors.length) {
+      index = 0;
+    }
+
+    $("#hereLink").css("color",colors[index]);
+    setTimeout(function() {
+      flash(index + 1);
+    }, speed);
+    // setInterval(flash, speed);
+  }
+
+  flash(0);
+});
